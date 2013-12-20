3579c25dfac551516034d5fb38108201a2586146
hanu <hdamodaran@thoughtworks.com>
2013-12-20 18:44:05 +0530

    Arduino code update

---
+++ Arduino/LM35TempRead/LM35TempRead.ino
@@ -0,0 +1,27 @@
+#include <Wire.h>
+
+#define SLAVE_ADDRESS 0x04
+
+float temperature;
+int reading;
+int tempPin = 0;
+
+void setup()
+{
+  Serial.begin(9600);
+  Wire.begin(SLAVE_ADDRESS);
+  Wire.onRequest(sendData);
+}
+
+void loop()
+{
+reading = analogRead(tempPin);
+temperature = reading * 0.488;
+}
+
+void sendData(){
+    Wire.write(temperature);
+}
+
+
+
